e741be7922ba8eaa310028a7abe21f51f3b70aad
halyna.vasylenko <halyna.vasylenko.cpe@nure.ua>
2017-12-04 19:54:53 +0300

    Format class

--- userH/src/test/java/ua/nure/HalenaVasylenko/UserTest.java
+++ userH/src/test/java/ua/nure/HalenaVasylenko/UserTest.java
@@ -6,11 +6,11 @@ import junit.framework.TestCase;
 
 public class UserTest extends TestCase {
 	@Test
-      public void testFullName (){
-	User user = new User ();
-	user.setFirstName("Halyna");
-	user.setLastName("Vasylenko");
-	String fullName = user.getFullName ();
-	assertEquals("Halyna Vasylenko", fullName);
+	public void testFullName() {
+		User user = new User();
+		user.setFirstName("Halyna");
+		user.setLastName("Vasylenko");
+		String fullName = user.getFullName();
+		assertEquals("Halyna Vasylenko", fullName);
 	}
 }
